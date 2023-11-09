519fb29e3e23b586a126800eaf4799b7feca2c6c
Uruena2603 <alejourus2003@gmail.com>
2023-11-09 17:33:11 -0500

    Inclusión texto.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Juan Alejandro Urueña + Juan David García</h1>
+      <h2>14 Champions</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
