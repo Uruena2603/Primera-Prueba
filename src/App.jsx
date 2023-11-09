0a4633b3935ff542c4dc9d0bde43385bfa455d99
Uruena2603 <alejourus2003@gmail.com>
2023-11-09 17:23:36 -0500

    Se actualiza color fondo.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div style={{backgroundColor: "gray"}}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
